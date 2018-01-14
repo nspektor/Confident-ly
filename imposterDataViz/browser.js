98c9f973ce288007d31f924a64395e11bfe0434f
Amir <al3526@columbia.edu>
2018-01-14 16:29:18 -0500

    added 'side' pane

--- imposterDataViz/browser.js
+++ imposterDataViz/browser.js
@@ -1,4 +1,22 @@
-const dataSet2 = d3.csv('Question1.csv', function(data){
-  console.log(data)
+// visualizing data from the csv file
+const svg = d3.select('svg')
+
+const dataSet2 = d3.csv('/Question1.csv',function(data){
+    console.log(data)
+    var groups = svg
+    .selectAll('g')
+    .data(data);
+
+    var texts = groups
+    .enter()
+    .append('g')
+    .append('text');
+
+    texts
+    .attr('transform', function(d, idx){
+        return 'translate(0,'+idx*20 + ')'
+    })
+    .text(function(d, idx){
+        return Object.keys(d)[0]
+    })
 })
-console.log(dataSet2)
