11b68abf006e0360b4906b8c9f6d8e9dff10e533
Nellie Spektor <nellie.spektor@gmail.com>
2018-01-14 16:00:03 -0500

    activities???

--- test/app.js
+++ test/app.js
@@ -41,6 +41,14 @@ describe('GET /contact', () => {
   });
 });
 
+describe('GET /activities', () => {
+  it('should return 200 OK', (done) => {
+    request(app)
+      .get('/activites')
+      .expect(200, done);
+  });
+});
+
 describe('GET /random-url', () => {
   it('should return 404', (done) => {
     request(app)
